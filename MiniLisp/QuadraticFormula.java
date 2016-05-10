48ee20af556f11fb467a09ed7437342914a53f94
Josh Frankl <jfrankl@utexas.edu>
2016-05-10 12:34:34 -0500

    Fixed Quadratic Formula error

--- MiniLisp/QuadraticFormula.java
+++ MiniLisp/QuadraticFormula.java
@@ -1,10 +1,10 @@
 public class QuadraticFormula
 {
     public static double quadPlus(double a, double b, double c) {
-        return (-1*b)+Math.sqrt(((b*b) -4*a*c) / (2*a));
+        return (-1*b)+Math.sqrt(((b*b) -4*a*c)) / (2*a);
     }
 
     public static double quadMinus(double a, double b, double c) {
-        return (-1*b)-Math.sqrt(((b*b) -4*a*c) / (2*a));
+        return (-1*b)-Math.sqrt(((b*b) -4*a*c)) / (2*a);
     }
 }
